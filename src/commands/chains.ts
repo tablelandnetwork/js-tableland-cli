f38c22ac731b3f8b222a6ae8f621741a0716c6bc
Carson Farmer <carson.farmer@gmail.com>
2022-06-15 15:23:54 -0700

    feat: add command to list supported chains
    Signed-off-by: Carson Farmer <carson.farmer@gmail.com>

---
+++ src/commands/chains.ts
@@ -0,0 +1,18 @@
+import type { Arguments, CommandBuilder } from "yargs";
+import { SUPPORTED_CHAINS } from "@tableland/sdk";
+
+type Options = {
+  // emopty
+};
+
+export const command = "chains";
+export const desc = "List information about supported chains";
+
+export const builder: CommandBuilder<Options, Options> = (yargs) => yargs;
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const handler = async (_argv: Arguments<Options>): Promise<void> => {
+  const out = JSON.stringify(SUPPORTED_CHAINS, null, 2);
+  console.log(out);
+  process.exit(0);
+};

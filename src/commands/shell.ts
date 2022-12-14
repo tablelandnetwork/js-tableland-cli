a296c9fffa622dc2ea9d16be69b4a852f04c2027
Allen Muncy <allen@textile.io>
2022-12-13 19:40:23 -0600

    Updated formatted, retain history, confirm writes

--- src/commands/shell.ts
+++ src/commands/shell.ts
@@ -2,26 +2,42 @@ import type { Arguments, CommandBuilder } from "yargs";
 import cliSelect from 'cli-select';
 import {
   connect,
-  resultsToObjects,
   ConnectOptions,
   ChainName,
+  Connection,
 } from "@tableland/sdk";
+// @ts-ignore 
 import chalk from 'chalk';
 import yargs from "yargs";
-import { promises } from "fs";
 import { createInterface } from "readline";
-import { getChains } from "../utils.js";
+import { getChains, getWalletWithProvider } from "../utils.js";
+// @ts-ignore 
+import init from "@tableland/sqlparser";
+import { table } from "table";
 
+function formatForDisplay(table: any): (string | number | unknown)[][] {
+  const result = table.rows;
+
+  const columns: string[] = [];
+  table.columns.forEach((column:any) => {
+    columns.push(column.name);
+  });
+  result.unshift(columns);
+
+  
+  return result;
+}
 
 
 export type Options = {
   // Local
   statement?: string;
   format: "pretty" | "table" | "objects";
-  file?: string;
 
   // Global
   chain: ChainName;
+  privateKey: string;
+  providerUrl: string | undefined;
 };
 
 export const command = "shell [statement]";
@@ -30,7 +46,7 @@ export const aliases = ["r", "query", "q"];
 
 async function confirmQuery() {
   const selected = await cliSelect({
-    values: {major: "Confirm write queries", minor: "Fire away, what's a few eth anyway?"},
+    values: {confirm: "Confirm: Send this transaction to the network", deny: "Oops. No, don't send that transaction."},
     valueRenderer: (value, selected) => {
       if (selected) {
         return chalk.underline(value);
@@ -38,76 +54,91 @@ async function confirmQuery() {
       return value;
     }
   });
-  console.log(chalk.bgBlue(selected.value));
-  return selected.id;
+console.log(chalk.bgBlue(selected.id));
+if(selected.id ==="confirm") {
+  console.log(chalk.underline("Committing to network. This will take a few moments."));
+  return true;
 }
 
-async function shellYeah(argv:any) {
-  let { statement } = argv;
-  const { format, chain, file } = argv;
-  const options: ConnectOptions = {
-    chain,
-  };
+return false;
+}
 
+async function fireFullQuery(statement: string, argv: any, tablelandConnection: Connection ) {
+  const { format } = argv;
+
+  switch(true) {
+    case statement.trim().endsWith(".help"): 
+      console.log("Uh, I didn't think I'd get this far");
+      break;
+    case statement.trim().endsWith(";"): 
+      // Parse query for read, write, or create;
+      // If write or create, confrm with cliSelect
+      // If read, return response in Tabular form
+      break;        
+  }
+
+    // @ts-ignore
+  const { type } = await globalThis.sqlparser.normalize(statement);
+
+  let res;
+  if(type==="write") {
+    const confirm = await confirmQuery();
+    if(confirm) {
+      res = await tablelandConnection.write(statement);
+      console.log(res);
+    }
+
+  } else {
+    res = await tablelandConnection.read(statement);
+  
+    // Defaults to "table" output format
+    // After we upgrade the SDK to version 4.x, we can drop some of this formatting code
+    if (format === "pretty") {
+      const formatted = formatForDisplay(res);
+      console.log(table(formatted, {
+        columns: [{
+          alignment: "center"
+        }]
+      }));
+    } else {
+      const out = JSON.stringify(res, null, 2);
+      console.log(out);
+    }
+    /* c8 ignore next 3 */
+
+  }
+}
+
+
+async function shellYeah(argv:any, tablelandConnection: Connection, history: string[] = []) {
+  
   try {
-    if (file != null) {
-      statement = await promises.readFile(file, { encoding: "utf-8" });
-    } else if (statement == null) {
+      let statement = "";
+      const rl = createInterface({ history, input: process.stdin, output: process.stdout, prompt: "tableland>", terminal: true });
+      rl.prompt();
+      rl.on('history', (newHistory) => {
+        history = newHistory;
+      })
 
-      statement = "";
-      const rl = createInterface({ input: process.stdin });
+      for await (const enter of rl) {    
 
-      const it = rl[Symbol.asyncIterator]();
-      
-      for await (const enter of it) {       
-        
         const state = enter;
         // @ts-ignore
         statement += '\r\n';
         statement += state;
+        rl.setPrompt('      ...>');
         if(state.trim().endsWith(";")) {
-          rl.close();
-          break;          
+          break;
         } 
       }
-
-      switch(true) {
-        case statement.trim().endsWith(".help"): 
-          console.log("Uh, I didn't think I'd get this far");
-          break;
-        case statement.trim().endsWith(";"): 
-          // Parse query for read, write, or create;
-          // If write or create, confrm with cliSelect
-          // If read, return response in Tabular form
-          break;
-        
-        
-      }
-
-    }
-    if (!statement) {
-      console.error(
-        chalk.cyanBright("missing input value (`statement`, `file`, or piped input from stdin required)")
-      );
-      return;
-    }
-    const res = await connect(options).read(statement);
-    // Defaults to "table" output format
-    // After we upgrade the SDK to version 4.x, we can drop some of this formatting code
-    const formatted = format === "table" ? res : resultsToObjects(res);
-
-    if (format === "pretty") {
-      console.table(formatted);
-    } else {
-      const out = JSON.stringify(formatted, null, 2);
-      console.log(out);
-    }
-    /* c8 ignore next 3 */
+      await fireFullQuery(statement, argv, tablelandConnection);
+  
+      shellYeah(argv, tablelandConnection, history);
   } catch (err: any) {
     console.error(err.message);
   }
 
-  shellYeah(argv);
+  
 }
 
 
@@ -122,15 +153,24 @@ export const builder: CommandBuilder<{}, Options> = (yargs) =>
       choices: ["pretty", "table", "objects"] as const,
       description: "Output format. One of 'pretty', 'table', or 'objects'.",
       default: "table",
-    })
-    .option("file", {
-      type: "string",
-      alias: "f",
-      description: "Get statement from input file",
     }) as yargs.Argv<Options>;
 
 export const handler = async (argv: Arguments<Options>): Promise<void> => {
-  const { chain } = argv;
+  await init();
+  const { privateKey, chain, providerUrl } = argv;
+  console.log(chain);
+
+  const signer = getWalletWithProvider({
+    privateKey,
+    chain,
+    providerUrl,
+  });
+  const options: ConnectOptions = {
+    chain,
+    signer,
+  };
+
+  const tablelandConnection = await connect(options);
 
   const network = getChains()[chain];
   if (!network) {
@@ -140,7 +180,7 @@ export const handler = async (argv: Arguments<Options>): Promise<void> => {
   console.log("Welcome to Tableland");
   console.log("Tableland CLI version 0.2.1");
   console.log(`Enter ".help" for usage hints`);
-  console.log("Connect to Polygon Mumbai using 0x0000");
+  console.log(`Connected to ${network.phrase} using ${signer.address}`);
 
-  await shellYeah(argv);
+  await shellYeah(argv, tablelandConnection);
 };

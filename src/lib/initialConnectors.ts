c40637e09ba4b42ebe9d45cecdd427317dc28b4d
Joe Wagner <njwjs722@gmail.com>
2023-01-23 13:15:22 -0700

    fix linting

--- src/lib/initialConnectors.ts
+++ src/lib/initialConnectors.ts
@@ -1,17 +1,18 @@
-import { Provider } from "@ethersproject/abstract-provider";
-import { ChainInfo, Database } from "@tableland/sdk";
-import { Signer, Wallet } from "ethers";
+// TODO: this stuff was declared here but never being used, commenting out so lint can pass -JW
+// import { Provider } from "@ethersproject/abstract-provider";
+// import { ChainInfo, Database } from "@tableland/sdk";
+// import { Signer, Wallet } from "ethers";
+// import EnsResolver from "./EnsResolver.js";
 import { GlobalOptions } from "../cli.js";
-import EnsResolver from "./EnsResolver.js";
 
-interface CliConnectors {
-  signer?: Signer;
-  provider?: Provider;
-  wallet?: Wallet;
-  database: Database;
-  ens: EnsResolver;
-  chain: ChainInfo;
-}
+// interface CliConnectors {
+//   signer?: Signer;
+//   provider?: Provider;
+//   wallet?: Wallet;
+//   database: Database;
+//   ens: EnsResolver;
+//   chain: ChainInfo;
+// }
 
 export default async function initializeConnectors(
   argv: GlobalOptions
